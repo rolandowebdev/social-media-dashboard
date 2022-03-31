75e8785023adea2cc8820e5de333212a45816a89
RolandoPranataAR <rolandopranata@gmail.com>
2022-04-01 00:08:41 +0700

    add: Create toggle function dark & light mode

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,43 @@
-console.log('Hello World');
+const radioButton = document.querySelectorAll('.toggle__wrapper input');
+const darkBtn = document.getElementById('dark');
+const ligthBtn = document.getElementById('light');
+
+const setColorMode = () => {
+    console.log(localStorage.getItem('colorMode'));
+    if (localStorage.getItem('colorMode') == 'light') {
+        setLightMode();
+        ligthBtn.click();
+    } else if (localStorage.getItem('colorMode') == 'dark') {
+        setDarkMode();
+        darkBtn.click();
+    } else {}
+};
+
+const checkIsLight = () => {
+    if (window.matchMedia('(prefers-color-scheme: light)').matches) {
+        ligthBtn.click();
+    }
+};
+
+const setDarkMode = () => {
+    document.querySelector('body').classList = 'dark';
+};
+
+const setLightMode = () => {
+    document.querySelector('body').classList = 'light';
+};
+
+setColorMode();
+checkIsLight();
+
+radioButton.forEach(function(radio) {
+    radio.addEventListener('click', () => {
+        if (darkBtn.checked) {
+            setDarkMode();
+            localStorage.setItem('colorMode', 'dark');
+        } else {
+            setLightMode();
+            localStorage.setItem('colorMode', 'light');
+        }
+    });
+});
